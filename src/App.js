5564ecd4dc4b824a4217dfe9b492a110fa7679bc
wallzack <vulcheek@gmail.com>
2020-01-23 22:24:44 +0100

    14.5 Added routing for Trips, Countries and Regions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
-// TODO - import other views
+// DONE - import other views
+import Countries from './components/views/Countries/CountriesContainer';
+import Regions from './components/views/Regions/RegionsContainer';
+import Country from './components/views/Country/CountryContainer';
+import Trip from './components/views/Trip/TripContainer';
+
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
@@ -40,7 +45,12 @@ class App extends React.Component {
           <Switch location={location}>
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
-            {/* TODO - add more routes for other views */}
+            {/* DONE - add more routes for other views */}
+            <Route exact path='/countries' component={Countries} />
+            <Route exact path='/regions' component={Regions} />
+            <Route exact path='/country' component={Country} />
+            <Route exact path='/trip' component={Trip} />
+            
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
           </Switch>
