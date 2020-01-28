07f77be0ba1f17ad186ff745b58e6315f818cfde
wallzack <vulcheek@gmail.com>
2020-01-28 23:26:08 +0100

    15.3 component name bug

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -15,7 +15,7 @@ const optionTypes = {
 
 const OrderOption = ({name, type, ...otherProps}) => {
   const OptionComponent = optionTypes[type];
-  if(!OptionComponent) {
+  if(!OptionComponent){
     return null;
   } else {
     return (
