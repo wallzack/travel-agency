d5cddc6e57b26f0f305e758083449db5b6ea4a11
wallzack <vulcheek@gmail.com>
2020-01-31 12:02:37 +0100

    15.3 updated OrderForm

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,23 +5,21 @@ import PropTypes from 'prop-types';
 import OrderOption from '../../features/OrderOption/OrderOption';
 import pricing from '../../../data/pricing.json';
 
-const OrderForm = ({options, tripCost, setOrderOption}) => {
-  return (
-    <Row>
-      {pricing.map(option => (
-        <Col key={option.id} md={4}>
-          <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption} />
-        </Col>
-      ))}
-      <Col xs={12}>
-        <OrderSummary cost={tripCost} options={options}/>
+const OrderForm = ({options, tripCost, setOrderOption}) => ( 
+  <Row> 
+    {pricing.map(option => (
+      <Col key={option.id} md={4}>
+        <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption} />
       </Col>
-    </Row>
-  );
-};
+    ))}
+    <Col xs={12}>
+      <OrderSummary cost={tripCost} options={options}/>
+    </Col>
+  </Row>
+);
 
 OrderForm.propTypes = {
-  tripCost: PropTypes.string,
+  tripCost: PropTypes.node,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
 };
