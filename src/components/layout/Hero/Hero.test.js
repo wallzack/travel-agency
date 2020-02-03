c8a01c8efc7016f5ca99e9e5e1419b8cc4b7f1d4
wallzack <vulcheek@gmail.com>
2020-02-03 22:02:46 +0100

    16.2 Completed initial it test

---
+++ src/components/layout/Hero/Hero.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Hero from './Hero';
+
+describe('Component Hero', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<Hero titleText='Lorem ipsum' imageSrc='test' />);
+    expect(component).toBeTruthy();
+    console.log(component.debug());
+  });
+  
+
+  it('should throw error without required props', () => {
+    expect (() => shallow(<Hero />)).toThrow();
+  });
+
+  it('should render correct title and image', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const expectedImage = 'image.jpg';
+    const component = shallow(<Hero titleText={expectedTitle} imageSrc={expectedImage} />);
+
+    const renderedTitle = component.find('.title').text();
+    expect(renderedTitle).toEqual(expectedTitle);
+    expect(component.find('.image').prop('src')).toEqual(expectedImage);
+  });
+});
