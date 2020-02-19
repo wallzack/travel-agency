53b99efec80e18ddd3d69109e463f2b228e6a5f5
wallzack <vulcheek@gmail.com>
2020-02-19 18:21:40 +0100

    17.2 added Test2: check if title and promoDescription are rendered

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -3,7 +3,10 @@ import React from 'react';
 class HappyHourAd extends React.Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <h3 className={'title'}></h3>
+        <p className={'promoDescription'}></p>
+      </div>
     );
   }
 }
--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
+const select = {
+  title: '.title',
+  promoDescription: '.promoDescription',
+};
+
 describe('Component HappyHourAd', () => {
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
+  });
+
+  it('should render heading and sescription', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.promoDescription)).toEqual(true);
   });
 });
