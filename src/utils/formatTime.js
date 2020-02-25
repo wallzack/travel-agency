6be09dde6ddbbb31a03edf2a01956fd014d5d2ff
wallzack <vulcheek@gmail.com>
2020-02-25 15:56:04 +0100

    17.3 task ready for review

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,1 +1,11 @@
-export const formatTime = () => 'formatted time';
+export const formatTime = seconds => {
+
+  if (seconds == null || isNaN(seconds) || seconds < 0) {
+    return null;
+  } else {
+    let date = new Date(null);
+    date.setSeconds(seconds);
+    const timeString = date.toISOString().substr(11, 8);
+    return timeString;
+  }
+};
